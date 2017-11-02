a468133f21fd5a83c79417b4eb7590252dca9d7f
Xavier Clerc <xclerc@janestreet.com>
2017-11-02 15:28:07 +0000

    Update for OCaml 4.06.

--- ppx/ppx_js_driver.ml
+++ ppx/ppx_js_driver.ml
@@ -1,5 +1,31 @@
+let of_405_impl =
+  (Migrate_parsetree_versions.migrate
+    (module Migrate_parsetree.OCaml_405)
+    (module Migrate_parsetree.OCaml_current)).copy_structure
+
+let to_405_impl =
+  (Migrate_parsetree_versions.migrate
+    (module Migrate_parsetree.OCaml_current)
+    (module Migrate_parsetree.OCaml_405)).copy_structure
+
+let of_405_intf =
+  (Migrate_parsetree_versions.migrate
+    (module Migrate_parsetree.OCaml_405)
+    (module Migrate_parsetree.OCaml_current)).copy_signature
+
+let to_405_intf =
+  (Migrate_parsetree_versions.migrate
+    (module Migrate_parsetree.OCaml_current)
+    (module Migrate_parsetree.OCaml_405)).copy_signature
+
 let () =
-  let js_mapper = Ppx_js.js_mapper [] in
+  let js_mapper = Ppx_js.mapper in
+  let impl x =
+    of_405_impl (js_mapper.structure js_mapper (to_405_impl x))
+  in
+  let intf x =
+    of_405_intf (js_mapper.signature js_mapper (to_405_intf x))
+  in
   Ppx_driver.register_transformation_using_ocaml_current_ast "js_of_ocaml"
-    ~impl:(js_mapper.Ast_mapper.structure js_mapper)
-    ~intf:(js_mapper.Ast_mapper.signature js_mapper)
+    ~impl
+    ~intf
