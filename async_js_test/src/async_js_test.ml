fab60dc8b1a9783a97a40ae17ae1024973120b2b
public-release <as-public-release@janestreet.com>
2023-11-15 15:50:15 +0000

    v0.17~preview.128.47+23

--- async_js_test/src/async_js_test.ml
+++ async_js_test/src/async_js_test.ml
@@ -5,16 +5,24 @@ module Expect_test_config = struct
 
   external loop_while : (unit -> bool Js.t) Js.callback -> unit = "loop_while"
 
-  let run f =
-    let result = Monitor.try_with f in
-    loop_while
-      (Js.wrap_callback (fun () ->
-         Async_kernel_scheduler.Expert.run_cycles_until_no_jobs_remain ();
-         Js.bool (not (Deferred.is_determined result))));
-    match Deferred.peek result with
-    | Some (Ok result) -> result
-    | Some (Error exn) -> raise exn
-    | None -> assert false
+  let is_in_browser = Js.Optdef.test (Obj.magic Dom_html.document : _ Js.Optdef.t)
+
+  let run =
+    if is_in_browser
+    then (fun f ->
+      Async_js.init ();
+      don't_wait_for (f ()))
+    else
+      fun f ->
+      let result = Monitor.try_with f in
+      loop_while
+        (Js.wrap_callback (fun () ->
+           Async_kernel_scheduler.Expert.run_cycles_until_no_jobs_remain ();
+           Js.bool (not (Deferred.is_determined result))));
+      match Deferred.peek result with
+      | Some (Ok result) -> result
+      | Some (Error exn) -> raise exn
+      | None -> assert false
   ;;
 
   let sanitize s = s
