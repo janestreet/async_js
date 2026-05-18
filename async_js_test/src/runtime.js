17ddb9c9ab88e13908fdf9e8604f46f9bb7120f1
public-release <as-public-release@janestreet.com>
2026-05-18 12:36:23 +0100

    v0.18~preview.130.100+614

--- async_js_test/src/runtime.js
+++ async_js_test/src/runtime.js
@@ -1,14 +1,21 @@
 //Provides: deasync
-var deasync = require('deasync');
+var deasync = {
+  loopWhile: (function (f) {
+    throw new Error("async_js_test has been linked, but the deasync npm package is not available");
+  })
+}
+if (typeof require !== 'undefined') {
+  deasync = require('deasync');
+}
 
 //Requires: deasync
 //Provides: loop_while
 function loop_while(f) {
-    deasync.loopWhile(f);
-    return 0;
+  deasync.loopWhile(f);
+  return 0;
 }
 
 //Provides: caml_wasm_suspend_available
 function caml_wasm_suspend_available() {
-    return +!!globalThis.WebAssembly.Suspending;
+  return +!!globalThis.WebAssembly.Suspending;
 };
